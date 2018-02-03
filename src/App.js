f8e6a4b19a487ca99c953991a15dacad2fdec0af
Alexander Müller <alexander.mueller@codecentric.de>
2018-02-03 11:46:19 +0100

    Added demo page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Typeahead from 'typereact';
+import './Typeahead.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        const groups = [
+            {label: 'Group 1', value: 'group1'},
+            {label: 'Group 2', value: 'group2'},
+            {label: 'Group 3', value: 'group3'},
+            {label: 'Empty Group', value: 'group4'}
+        ];
+        const options = [
+            {label: 'First Label', value: 'value1', group: 'group1'},
+            {label: 'Second Label', value: 'value2', group: 'group2'},
+            {label: 'Third Label', value: 'value3', group: 'group3'},
+            {label: 'Fourth Label', value: 'value4', group: 'group1'},
+            {label: 'Fifth Label', value: 'value5', group: 'group2'},
+            {label: 'Sixth Label', value: 'value6', group: 'group3'},
+            {label: 'Seventh Label', value: 'value7', group: 'group1'}
+        ];
+        const GROUP_COUNT = 10;
+        const OPTION_COUNT = 100;
+        const generateGroups = () => {
+            const generatedGroups = [];
+            for (let groupIndex = 1; groupIndex <= GROUP_COUNT; groupIndex++) {
+                generatedGroups.push({label: `Group ${groupIndex}`, value: `group${groupIndex}`});
+            }
+            return generatedGroups;
+        };
+        const generateOptions = () => {
+            const generatedOptions = [];
+            for (let optionIndex = 1; optionIndex <= OPTION_COUNT; optionIndex++) {
+                generatedOptions.push({
+                    label: `Label ${optionIndex}`,
+                    value: `value${optionIndex}`,
+                    group: `group${((optionIndex - 1) % GROUP_COUNT) + 1}`
+                });
+            }
+            return generatedOptions;
+        };
+        return (
+            <div style={{display: 'flex'}}>
+                <div style={{display: 'block', margin: '30px', width: '300px'}}>
+                    <h1>Normal</h1>
+                    <h2>Default</h2>
+                    <Typeahead fieldName="fieldNameStandard1" options={options}/>
+                    <div style={{height: '86px'}}></div>
+                    <h2>allowUnknownValue = true</h2>
+                    <Typeahead fieldName="fieldNameStandard2" options={options} allowUnknownValue={true}/>
+                    <h2>isClearable = true</h2>
+                    <Typeahead fieldName="fieldNameStandard3" options={options} isClearable={true}/>
+                    <h2>allowUnknownValue = true, isClearable = true</h2>
+                    <Typeahead fieldName="fieldNameStandard4" options={options} allowUnknownValue={true}
+                        isClearable={true}/>
+                    <h2>autoSelectSingleOption = true</h2>
+                    <Typeahead fieldName="fieldNameStandard5" options={[options[0]]} autoSelectSingleOption={true}/>
+                    <h2>Many Options</h2>
+                    <Typeahead fieldName="fieldNameStandard6" options={generateOptions()}/>
+                </div>
+                <div style={{display: 'block', margin: '30px', width: '300px'}}>
+                    <h1>Grouped</h1>
+                    <h2>Default</h2>
+                    <Typeahead fieldName="fieldNameGroup1" options={options} groups={groups}/>
+                    <h2>renderEmptyGroups = true</h2>
+                    <Typeahead fieldName="fieldNameGroup2" options={options} groups={groups} renderEmptyGroups={true}/>
+                    <h2>allowUnknownValue = true</h2>
+                    <Typeahead fieldName="fieldNameGroup3" options={options} groups={groups} allowUnknownValue={true}/>
+                    <h2>isClearable = true</h2>
+                    <Typeahead fieldName="fieldNameGroup4" options={options} groups={groups} isClearable={true}/>
+                    <h2>allowUnknownValue = true, isClearable = true</h2>
+                    <Typeahead fieldName="fieldNameGroup5" options={options} groups={groups} allowUnknownValue={true}
+                        isClearable={true}/>
+                    <h2>autoSelectSingleOption = true</h2>
+                    <Typeahead fieldName="fieldNameGroup6" options={[options[0]]} groups={groups}
+                        autoSelectSingleOption={true}/>
+                    <h2>Many Options</h2>
+                    <Typeahead fieldName="fieldNameGroup7" options={generateOptions()} groups={generateGroups()}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
